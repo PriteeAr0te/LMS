53c2fc233507a36d777d7cc1568212881203fe99
PriteeAr0te <arotepritee111@gmail.com>
2024-01-10 13:16:01 +0530

    Set up initial backend routes and models

---
+++ backend/models/Admin.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const AdminSchema = new Schema({
+    name : {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phone: {
+        type: Number,
+        required: true
+    },
+    Profession: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('admin', AdminSchema)
---
+++ backend/models/Course.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const CourseSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    instructor: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: String,
+    },
+    tags: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('course', CourseSchema)
