5eacc60d4ca9a46a28970c1de9066e4b421c45f1
priteEDreams <pritee.arote@dreamsinternational.in>
2024-03-22 23:28:00 +0530

    Updated index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
 const connectToMongo = require('./db');
 const express = require('express')
 const cors = require('cors')
+require("dotenv").config();
 
 
 const app = express();
+// app.use(
+//   cors({
+//     origin: ["https://lms-plum-sigma.vercel.app/"],
+//     methods: ["POST", "GET"],
+//     credentials: true,
+//   })
+// );
 app.use(cors())
 
 const port = 5000;
@@ -12,6 +20,9 @@ connectToMongo();
 
 //use this middleware if you are using req.body
 app.use(express.json())
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 // Available Routes 
 app.use('/api/auth', require('./routes/auth'))
